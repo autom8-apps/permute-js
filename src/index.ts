ecee8e2b0490a53e0a97356e2479bcf192253faf
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2020-08-16 13:48:31 -0600

    updating version, permute import

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { omit, isObject, isPlainObject, once, hasIn, zipObject, pick, mapKeys, merge, flatMap } from "lodash";
-import { Validator, ReShaper, ShaperStrategy } from "./shapers/index";
-import { LodashUtils, ISettings, IStrategy } from "./shapers/interfaces";
-const _: LodashUtils = Object.freeze({ omit, isObject, isPlainObject, hasIn, zipObject, pick, mapKeys, merge, flatMap });
+const { omit, isObject, isPlainObject, once, hasIn, zipObject, pick, mapKeys, merge, flatMap } = require("lodash");
+const { Validator, ReShaper, ShaperStrategy } = require("./shapers/index");
+const { LodashUtils, ISettings, IStrategy } = require("./shapers/interfaces");
+const _: typeof LodashUtils = Object.freeze({ omit, isObject, isPlainObject, hasIn, zipObject, pick, mapKeys, merge, flatMap });
 
 const strategy = new ShaperStrategy(_);
 strategy.setStrategy(new Validator(_));
 strategy.setStrategy(new ReShaper(_));
 
-function permute(data: object | object[], settings: ISettings) {
+function permute(data: object | object[], settings: typeof ISettings) {
   return strategy.operate(data, settings);
 }
 
